f3bb1ea6d3dacb2719039661670dd9cfaa17d2e1
Adarsh Jagan S <adarshjagan3895@gmail.com>
2018-10-02 10:11:31 -0400

    app/AnalogSensor: Used unique ptr to solve data leak error.

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,6 +1,8 @@
 #include <AnalogSensor.hpp>
+#include<memory>
 #include <numeric>
 #include <vector>
+
 
 AnalogSensor::AnalogSensor(unsigned int samples)
     : mSamples(samples)
@@ -12,8 +14,9 @@ AnalogSensor::~AnalogSensor()
 }
 
 int AnalogSensor::Read()
-{
-    std::vector<int> *readings = new std::vector<int>(mSamples, 10);
+{  // Data leak was found in following line. Fixed using smart pointer.
+  std::unique_ptr<std::vector<int>> readings =
+      std::make_unique<std::vector<int>>(mSamples, 10);
 
     double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
     return result;
