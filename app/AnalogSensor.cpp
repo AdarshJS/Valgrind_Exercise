23aecd618ac29b76a3d04fdd37ea72463688c8ee
Adarsh Jagan S <adarshjagan3895@gmail.com>
2018-10-02 12:05:56 -0400

    app: Made changes to rectify cpplint and cppcheck errors.

--- app/AnalogSensor.cpp
+++ app/AnalogSensor.cpp
@@ -1,3 +1,10 @@
+/*
+ * @file       AnalogSensor.cpp
+ * @author     Adarsh Jagan S
+ * @copyright  2018 Adarsh Jagan S
+ * @brief      Example code for Valgrind exercise.
+ *             
+ */
 #include <AnalogSensor.hpp>
 #include<memory>
 #include <numeric>
@@ -5,20 +12,18 @@
 
 
 AnalogSensor::AnalogSensor(unsigned int samples)
-    : mSamples(samples)
-{
+    : mSamples(samples) {
 }
 
-AnalogSensor::~AnalogSensor()
-{
+AnalogSensor::~AnalogSensor() {
 }
 
-int AnalogSensor::Read()
-{  // Data leak was found in following line. Fixed using smart pointer.
+int AnalogSensor::Read() {
+  // Data leak was found in following line. Fixed using smart pointer.
   std::unique_ptr<std::vector<int>> readings =
       std::make_unique<std::vector<int>>(mSamples, 10);
 
-    double result = std::accumulate( readings->begin(), readings->end(), 0.0 ) / readings->size();
+    double result = std::accumulate(readings->begin(), readings->end(), 0.0) / readings->size();
     return result;
 }
 
--- app/main.cpp
+++ app/main.cpp
@@ -1,14 +1,18 @@
+/*
+ * @file       main.cpp
+ * @author     Adarsh Jagan S
+ * @copyright  2018 Adarsh Jagan S
+ * @brief      Example program for valgrind assignment
+ */
 #include <iostream>
 #include <AnalogSensor.hpp>
 
-int main()
-{
+int main() {
     AnalogSensor lightSensor(5);
     std::cout << "Averaged sensor reading: " << lightSensor.Read() << std::endl;
   // Initialized terminator to solve valgrind error.
   bool terminator = true;
-    if( terminator )
-    {
+    if (terminator) {
         std::cout << "DONE" << std::endl;
     }
     return 0;
